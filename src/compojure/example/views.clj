6df40ea0bc85d60f22989fe5df66f52e81c26570
Antoni Batchelli <tbatchelli@acm.org>
2014-07-29 15:33:38 -0700

    Show ipv4 addresses in homepage.

--- src/compojure/example/views.clj
+++ src/compojure/example/views.clj
@@ -1,10 +1,21 @@
 (ns compojure.example.views
   (:use [hiccup core page]))
 
+(defn local-addresses []
+   (->> (java.net.NetworkInterface/getNetworkInterfaces)
+        enumeration-seq
+        (map bean)
+        (filter (complement :loopback))
+        (mapcat :interfaceAddresses)
+        ;; hopefully this eliminates ipv6 addresses
+        (filter #(some (fn [x] (= x (.getNetworkPrefixLength %))) [8,16,24]))
+        (map #(.. % (getAddress) (getHostAddress)))))
+
 (defn index-page []
   (html5
-    [:head
-      [:title "Hello World"]
-      (include-css "/css/style.css")]
-    [:body
-      [:h1 "Hello World"]]))
+   [:head
+    [:title "Hello World"]
+    (include-css "/css/style.css")]
+   [:body
+    [:h1 (str "Hello World from: "
+              (apply str (interpose ", " (local-addresses))))]]))
